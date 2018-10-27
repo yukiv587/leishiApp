85eaeed4c10114bb6b107b77b3180a4bd987f786
yuancheng <1963312192@qq.com>
2018-10-27 18:58:43 +0800

    1

---
+++ pages/index/job/list.js
@@ -0,0 +1,88 @@
+// pages/index/job/list.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    jobs: [],
+    mediaPrefix: getApp().globalData.mediaPrefix
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    getApp().loginPromise.then(() => {
+      this.getList();
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+
+  //获取列表
+  getList() {
+    getApp().http({
+      url: getApp().globalData.api.articles,
+      data: {
+        sid: 56,
+      },
+      success: res => {
+        res.data.map((item) => {
+          let now = new Date(item.t * 1000);
+          item.time = now.getFullYear() + '-' + (parseInt(now.getMonth()) + 1) + '-' + now.getDate();
+        });
+        this.setData({
+          jobs: res.data,
+        });
+      }
+    });
+  }
+})
